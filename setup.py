42d14ba04f04856a7e6646feea8f47f653705837
James Douglass <jdouglass@stanford.edu>
2024-09-16 15:39:06 -0700

    Initial commit of build scripts.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from distutils.extension import Extension
+
+import numpy
+from Cython.Distutils import build_ext
+
+ext_modules = [
+    Extension(
+        'seals.seals_cython_functions',
+        ['seals/seals_cython_functions.pyx'],
+    )
+]
+
+returned = setup(
+    name='seals_cython_functions',
+    packages=["seals"],
+    include_dirs=[numpy.get_include()],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules
+)
